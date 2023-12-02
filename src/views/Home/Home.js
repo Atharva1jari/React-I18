8c24221339002d0917db7c94f8da82d049cb7719
atharva1jari <atharvawanjari09@gmail.com>
2023-12-02 10:00:11 +0530

    Added languages file

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,8 +2,19 @@ import React from 'react';
 import "./Home.css"
 
 function home() {
+
+    const usersCount = 10;
     return (
-        <div>home</div>
+        <div>
+            <h1>Welcome</h1>
+            <p>We hope you enjoy learning React.</p>
+
+            <h3>Have a great day!</h3>
+             
+            <p>
+               {usersCount} users are learning in this seassion.
+            </p>
+        </div>
     )
 }
    
