84c2209785cf29e01bbfcde5b1b92f9799be2812
atharva1jari <atharvawanjari09@gmail.com>
2023-12-06 08:59:02 +0530

    added css

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,13 +8,28 @@ function home() {
  
     return (
         <div>
-            <h1>{I18n("welcomeMessage")}</h1>
+            <h1 className='heading'>I18n....</h1>
+            <div className='heading1'>
+            <h1 >{I18n("welcomeMessage")}</h1>
 
-            <p>{I18n("normalMessage")}</p>
+            <p >{I18n("normalMessage")}</p>
 
-            <h3>{I18n("greetingMessage")}</h3>
+            <h3 >{I18n("greetingMessage")}</h3>
 
+            </div>
             <select 
+                defaultValue={localStorage.getItem("lang")}
+                onChange={(e) => {
+                    localStorage.setItem("lang", e.target.value);
+                    window.location.reload();
+                }}
+                className="text-container">
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+                <option value="mr">Marathi</option>
+            </select>
+
+           {/* <select 
             defaultvalue={localStorage.getItem("lang")} 
             onChange={(e)=>{
                 localStorage.setItem("lang", e.target.value);
